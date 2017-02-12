0e02b7738fa3c6156d0bb581c1e499006a2a762d
bens-git <benjaminsaddress@gmail.com>
2017-02-11 19:23:11 -0500

    Personalized

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,4 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -34,13 +21,17 @@ var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+//var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img class="projectImage" src="%data%">';
+
+
+
+
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -52,42 +43,22 @@ var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineAuthor = '<div>%data%</div>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+//var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
-/*
-The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('button').click(function() {
     var $name = $('#name');
     var iName = inName($name.text()) || function(){};
     $name.html(iName);
   });
-});
+});*/
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
-var clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
 
 
 
@@ -234,16 +205,14 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,260 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Benjamin Small",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "226-388-4595",
+        "email": "bensmall.developer@gmail.com",
+        "github": "bens-git",
+        "location": "Brantford, Ontario"
+    },
+    "skills": [
+        "PHP",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "JQuery",
+        "Databases (MySQL, MSSQL)",
+        "MVC frameworks",
+        "Documentation"
+    ],
+    "welcomeMsg": "Welcome",
+    "bioPic": "images/Ben.jpg"
+
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Activation Laboratories",
+            "title": "Senior Lead Programmer",
+            "location": "Ancaster, Ontario",
+            "dates": "2009 - Present (7.5 years)",
+            "description": [
+                "Planning and maintaining the underlying architecture of the software",
+
+                "Overseeing the work being done by any other software engineers working on the project",
+
+                "Mentoring new or lower-level software developers or programmers, as well as all the members on the development team",
+
+                "Interfacing between the programmers and management",
+
+                "Supervisorial responsibilities in delegating work and ensuring that software projects come in on time and under budget",
+
+                "Providing technical advice and programming perspectives to management on requirements"]
+        },
+        {
+            "employer": "Andrews Lab, McMaster University",
+            "title": "Research Assistant",
+            "location": "Hamilton, Ontario",
+            "dates": "2007- 2008 (1 year)",
+            "description": [
+                "Designing and Maintaining the lab's website",
+
+                "Designing flash applications to visualize pathways leading to apoptosis in cancer treatments"
+
+            ]
+        }
+    ],
+
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "LIMS",
+            "dates": "2010 - Present (6 years)",
+            "description": "Intranet Based Laboratory Information Management System. A system for managing laboratory data. The system receives samples, allows for results to be input/imported, organizes instrument processes and lab jobs, and outputs reports and invoices.",
+            "images": ["images/2017-02-08_14-32-13.png", "images/2017-02-08_14-37-20.png"]
+        },
+        {
+            "title": "Scheduling Program",
+            "dates": "2016",
+            "description": "Database and PHP based program. Queries a database for all samples, statuses and processes in the system. Schedules all work based on algorithm that optimizes use of laboratory resources.",
+            "images": ["images/2017-02-08_14-33-42.png", "images/2017-02-08_14-35-08.png"]
+        },
+        {
+            "title": "Data automation",
+            "dates": "2012",
+            "description": "Automation of data manipulation processes that users previously did using excel. Data imported to a database. Operations and calculations done using php. Results displayed in browser.",
+            "images": ["images/2017-02-08_14-40-28.png", "images/2017-02-08_14-43-20.png"]
+        }
+
+    ]
+
+
+}
+
+var education = {
+    "schools": [
+        {
+            "school": "McMaster University",
+            "location": "Hamiltion, Ontario",
+            "degree": "Bachelors",
+            "major": "Science",
+            "dates": "2003-2008"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Learn to Program: The Fundamentals",
+            "dates": "Oct 2016",
+            "author": "University of Toronto",
+            "school": "Coursera",
+            "url": "Coursera",
+        },
+        {
+            "title": "Algorithms, Part I",
+            "dates": "Dec 2016",
+            "author": "Princeton University",
+            "school": "Coursera",
+            "url": "Coursera",
+        },
+        {
+            "title": "Front-End Web Developer Nanodegree",
+            "dates": "In Progress",
+            "author": "Udacity",
+            "school": "Udacity",
+            "url": "Udacity",
+        },
+    ]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
+bio.display = function () {
+    var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedemail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+    $("#topContacts").append(formattedMobile);
+    $("#topContacts").append(formattedemail);
+    $("#topContacts").append(formattedgithub);
+    $("#topContacts").append(formattedlocation);
+
+
+    var formattedbioPic = HTMLbioPic.replace("%data%", "images/Ben.jpg");
+    var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+
+    $("#header").append(formattedbioPic);
+    $("#header").append(formattedwelcomeMsg);
+    $("#header").append(HTMLskillsStart);
+
+
+
+
+    for (var i = 1; i < bio.skills.length; i++) {
+        var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedSkill)
+
+    }
+}
+
+work.display = function () {
+
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+
+        $(".work-entry:last").append("</ol></div>")
+            .append(formattedEmployerTitle)
+            .append(formattedDates)
+            .append(formattedWorkLocation)
+            .append("<div class='work-list'><ol>")
+
+        for (var i = 1; i < work.jobs[job].description.length; i++) {
+            $(".work-entry ol:last").append("<li>" + "<p>" + work.jobs[job].description[i] + "</p></li>")
+        }
+
+    }
+};
+
+projects.display = function () {
+    for (project in projects.projects) {
+        $("#projects").append(HTMLprojectStart);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+        $(".project-entry:last")
+            .append(formattedTitle)
+            .append(formattedDates)
+            .append(formattedDescription)
+            .append("<div class='img-container'>");
+
+
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".img-container:last").append("<div class='img-item'><a  class = 'thumbnail'>" + formattedImage + "</a></div>");
+            }
+        }
+        $(".project-entry:last")
+            .append("</div>");
+    }
+};
+
+$( document ).ready(function() {
+    $('.thumbnail').on('click', function() {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+        return false;
+    });
+});
+
+education.display = function () {
+    for (school in education.schools) {
+        $("#education").append(HTMLschoolStart);
+        var formattedSchool = HTMLschoolName.replace("%data%", education.schools[school].school);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+        $(".education-entry:last")
+            .append(formattedSchool)
+            .append(formattedDegree)
+            .append(formattedDates)
+            .append(formattedLocation)
+            .append(formattedMajor);
+
+    }
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+    for (course in education.onlineCourses) {
+
+        var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+        var formattedOSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+        var formattedAuthor = HTMLonlineAuthor.replace("%data%", education.onlineCourses[course].author);
+        var formattedODates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+        var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+
+        $(".education-entry:last")
+            .append(formattedTitle)
+            .append(formattedOSchool)
+            .append(formattedAuthor)
+            .append(formattedODates)
+            .append(formattedURL);
+
+    }
+};
+
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+//$('#main').append(internationalizeButton);
+
+$('#mapDiv').append(googleMap);
